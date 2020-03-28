2013ab22fdd916c9e23d6c7195580d053394432d
Cyril Carlin <cyrilcarlin0@gmail.com>
2020-03-28 22:42:10 +0100

    Home page skelleton

--- FrontEnd/BriscolaProject/src/app/app-routing.module.ts
+++ FrontEnd/BriscolaProject/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/BriscolaProject/src/app/app.module.ts
+++ FrontEnd/BriscolaProject/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { QuickSearchComponent } from './quick-search/quick-search.component';
+import { PlayMenuComponent } from './play-menu/play-menu.component';
+import { CreateGameComponent } from './play-menu/create-game/create-game.component';
+import { OngoingGamesComponent } from './play-menu/ongoing-games/ongoing-games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    QuickSearchComponent,
+    PlayMenuComponent,
+    CreateGameComponent,
+    OngoingGamesComponent
   ],
   imports: [
     BrowserModule,
