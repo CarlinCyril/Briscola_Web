b92dd21f5fcbdad11a5e751f2bbdb69f7892145d
Margherita Donnici <margheritadonnici@gmail.com>
2020-04-05 00:24:08 +0200

    Added docstrings to existing classes in the backend.

--- BackEnd/cards.py
+++ BackEnd/cards.py
@@ -1,11 +1,20 @@
 from typing import Set,List
 import random
 
-# Deck and Card classes
+# Card enums
 SUITS = ("bastoni", "denari", "spade", "coppe")
 VALUES = range(1,11)
 
 class Card:
+    """
+    Class which represents a card.
+
+    Attributes:
+    suit (str): suit of the card.
+    value (int): value of the card.
+    points (int): how many points the card is worth (in briscola).
+
+    """
     def __init__(self, suit: str, value: int) -> None:
         self.suit = suit
         self.value = value
@@ -24,10 +33,25 @@ class Card:
         else:
             self.points = 0
 
-    def isBriscola(self, briscolaSuit: str) -> bool:
-        return self.suit == briscolaSuit
+    def isSuit(self, suit: str) -> bool:
+        """
+        Given a suit as input, checks whether the card is of that suit or not.
+
+        Args:
+            suit: the suit to compare.
+
+        Returns:
+            True if the card is of the input suit, False otherwise.
+        """
+        return self.suit == suit
 
     def  __gt__(self, other) -> bool:
+        """
+        Override > operator for Cards: the value is taken into account, following briscola rank (A>3>10>9>8>7>6>5>4>2)
+
+        Returns:
+            True if the first card's rank is higher than the seconde one's, False otherwise.
+        """
         if (self.value in [1,3] and other.value in [1,3]):
             return self.value < other.value
         elif (not self.value in [1,3] and other.value in [1,3]):
@@ -39,9 +63,25 @@ class Card:
         
 
     def prettyPrint(self) -> None:
+        """
+        Method to pretty print the card's attributes.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
         print ("{} di {}, punti {}".format(self.value,self.suit,self.points))
     
 class Deck:
+    """
+    Class which represents a deck of cards.
+
+    Attributes:
+    cards (list of Card): list of cards in the deck.
+
+    """
     def __init__(self) -> None:
         self.cards = []
         # Build deck
@@ -50,16 +90,52 @@ class Deck:
                 self.cards.append(Card(suit,value))
 
     def shuffle(self) -> None:
+        """
+        Method to pretty print the card's attributes.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
         for i in range(len(self.cards) - 1, 0, -1):
             r = random.randint(0,i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
     def drawCard(self) -> Card:
+        """
+        Remove the first card of the deck and return it.
+
+        Args:
+            None.
+
+        Returns:
+            The card which has been removed.
+        """
         return self.cards.pop(0)
     
     def isEmpty(self) -> bool:
+        """
+        Check if the deck is empty.
+
+        Args:
+            None.
+
+        Returns:
+            True if there are no more cards in the deck, False otherwise.
+        """
         return not self.cards
     
     def prettyPrint(self) -> None:
+        """
+        Method to pretty print the cards in the deck.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
         for card in self.cards:
             card.prettyPrint()
--- BackEnd/game.py
+++ BackEnd/game.py
@@ -8,6 +8,22 @@ from cards import Card, Deck
 
 class Game:
 
+    """
+    Core class used to represent a game of Briscola.
+
+    Attributes:
+    deck (Deck): the deck of cards for the game.
+    id (str): unique id for the game (32-character hexadecimal string).
+    isFull (bool): True if the number of players of the game has been reached.
+    numPlayers (int): number of players of the game (can be 2,3,4 or 5).
+    teams (list of Team): list of the teams in the game (can be 2 or 3).
+    players (list of Player): list of all the players in the game.
+    briscola (str): briscola suit for the game.
+    currentPlayer (Player): the player whose turn it is currently.
+    currentDeal (Deal): the current Deal which is being played.
+
+    """
+
     deck = Deck()
 
     def __init__(self, player: Player, numPlayers: int) -> None:
@@ -23,22 +39,41 @@ class Game:
         self.players = [player]
         self.briscola = None
         self.currentPlayer = None
-        # Table is a list of (PlayerIndex,Card) tuples
-        self.table = []
+        self.currentDeal = None
 
-    def addPlayer(self, player: Player) -> None:
+    def addPlayer(self, player: Player) -> bool:
+        """ Adds a player to the game (if it is not full) and assigns it to a team
+
+        Args:
+            player: The player to add to the game.
+
+        Returns:
+            True if player has been added, False otherwise.
+
+        """
         if (not self.isFull):
             if (self.numPlayers == 2):
                 secondTeam = Team(player)
                 self.teams.append(secondTeam)
                 self.players.append(player)
                 self.isFull = True
+                return True
             #TODO: add elif for 3,4 and 5 players
         else:
             print("Cannot add player, game is full!")
+            return False
         
 
     def startGame(self) -> None:
+        """ Starts the game of briscola (shuffle deck and deal cards).
+
+        Args:
+            None.
+
+        Returns:
+            None.
+
+        """
         self.deck.shuffle()
         # Draw briscola and set it as the last card of the deck
         briscolaCard = self.deck.drawCard()
@@ -53,10 +88,34 @@ class Game:
         self.dealCards(starterPlayerIndex, 3, self.numPlayers)
 
     def dealCards(self, firstPlayer:int, numCards: int, numPlayers: int) -> None:
+        """ Deals cards to each player.
+
+        Args:
+            firstPlayer: the cards will be dealt starting by the player with this index.
+            numCards: the number of cards to be dealt.
+            numPlayers: the number of Players in the game.
+
+        Returns:
+            None.
+            
+        """
         for i in range(numCards * numPlayers):
             self.players[(i + firstPlayer)%numPlayers].addCardToHand(self.deck.drawCard())
 
-class Hand:
+class Deal:
+
+    """
+    A class used to represent a deal - the play from the time the cards are dealt until they are redealt.
+
+    Attributes:
+    leadSuit (str): lead suit of the deal (suit of the first played card)
+    briscolaSuit (str): briscola suit for the game
+    table (list of (int, Card)): list of the cards which are on the table, represented by tuples containing the card and the ID
+        of the player who played it
+    winner((int,Card)): tuple of the card who is currently winning the deal (is updated everytime card is added to the table)
+
+    """
+
     def __init__(self, firstCard: Card, playerIndex: int, briscola: str):
         self.leadSuit = firstCard.suit
         self.briscolaSuit = briscola
@@ -65,23 +124,45 @@ class Hand:
         self.winner = cardTuple
     
     def addCard(self,card: Card, playerIndex : int) -> None:
+        """ Adds a card to the table, and updates the deal's winner accordingly
+
+        Args:
+            card: The card to add to the table.
+            playerIndex: The id (index) of the player who played the card.
+
+        Returns:
+            None.
+
+        """
         cardTuple = (playerIndex, card)
         self.table.append(cardTuple)
-        #Determine winner
+        # If the new card wins over the current winner, update the winner
+        if (self.isNewWinner(card)):
+            self.winner = cardTuple
     
-    def isNewWinner(self, previousWinner: Card, newCard: Card) -> bool:
-        if (not previousWinner.isBriscola(self.briscolaSuit) and newCard.isBriscola(self.briscolaSuit)):
+    def isNewWinner(self, newCard: Card) -> bool:
+        """ Checks if a card wins over the current card winning the deal.
+
+        Args:
+            newCard: The card to compare against the current winning card.
+
+        Returns:
+            True if the card wins over the current winning card, False otherwise.
+
+        """
+        currentWinner = self.winner[1]
+        if (not currentWinner.isSuit(self.briscolaSuit) and newCard.isSuit(self.briscolaSuit)):
             return True
-        elif (previousWinner.isBriscola(self.briscolaSuit) and not newCard.isBriscola(self.briscolaSuit)):
+        elif (currentWinner.isSuit(self.briscolaSuit) and not newCard.isSuit(self.briscolaSuit)):
             return False
-        elif (previousWinner.isBriscola(self.briscolaSuit) and newCard.isBriscola(self.briscolaSuit)):
-            return newCard > previousWinner
+        elif (currentWinner.isSuit(self.briscolaSuit) and newCard.isSuit(self.briscolaSuit)):
+            return newCard > currentWinner
         else:
-            if(previousWinner.suit == self.leadSuit and newCard.suit == self.leadSuit):
-                return newCard > previousWinner
-            elif (previousWinner.suit == self.leadSuit and not newCard.suit == self.leadSuit):
+            if(currentWinner.suit == self.leadSuit and newCard.suit == self.leadSuit):
+                return newCard > currentWinner
+            elif (currentWinner.isSuit(self.leadSuit) and not newCard.isSuit(self.leadSuit)):
                 return False
-            elif (not previousWinner.suit == self.leadSuit and newCard.suit == self.leadSuit):
+            elif (not currentWinner.isSuit(self.leadSuit) and newCard.isSuit(self.leadSuit)):
                 return True
             else:
                 print("Both cards are not briscola nor lead suit - impossible!")
