13fdc8c44eee512a9c73d4b77a719bf06d551e16
Margherita Donnici <margheritadonnici@gmail.com>
2020-04-15 14:37:54 +0200

    Separated db logic in DbUtils, added GET game by id and POST game

--- BackEnd/briscolaWeb/errors.py
+++
@@ -1,9 +0,0 @@
-'''Application error handlers.'''
-
-from flask import Blueprint, jsonify, make_response
-
-bp = Blueprint('errors', __name__)
-
-@bp.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
